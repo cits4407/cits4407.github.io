567f81c86e5b0db038b05112e749329ef3fdd1c3
arran <arranstewart@users.noreply.github.com>
2021-04-01 14:42:36 +0800

    scripts

---
+++ copy_lectures.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+set -ex
+cp ../cits4407-sources/lectures/lect*pdf docs/lectures
---
+++ copy_workshops_and_sols.sh
@@ -0,0 +1,5 @@
+
+set -ex
+cp ../cits4407-sources/workshops/workshop[0-9][0-9]/workshop[0-9][0-9].pdf docs/workshops
+cp ../cits4407-sources/workshops/workshop[0-9][0-9]/workshop[0-9][0-9]-solutions.pdf docs/workshops
+
